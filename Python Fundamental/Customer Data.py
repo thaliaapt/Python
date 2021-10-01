48287bb6e2475a386925f30c69d25601fba299bf
Thalia Putri <91591220+thaliaapt@users.noreply.github.com>
2021-10-01 17:14:13 +0700

    Add files via upload

---
+++ Python Fundamental/Customer Data.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000} 
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000} 
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000} 
+daftar_belanja = [sepatu, baju, celana]
+
---
+++ Python Fundamental/Master.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000 }
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000 }
+
+
+# In[ ]:
+
+
+
+
